ee25166487972ed0ec73be48929f92567d3af9c8
Leila Ech-Chabi <leila.echchabi@gmail.com>
2020-06-11 10:39:18 +0200

    finalisation panier

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,11 +2,9 @@
 
 namespace App\Controller;
 
-use App\Repository\ProductRepository;
+
 use App\Service\cart\CartService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CartController extends AbstractController
@@ -14,30 +12,12 @@ class CartController extends AbstractController
     /**
      * @Route("/cart", name="cart")
      */
-    public function index(SessionInterface $session, ProductRepository $productRepository)
+    public function index(CartService $cartService)
     {
-        $panier = $session->get('panier',[]);
-        $panierWithData = [];
-
-        foreach ($panier as $id => $quantity){
-            $panierWithData[] = [
-                'watch'=> $productRepository->find($id),
-                'quantity'=>$quantity
-            ];
-        }
-
-//        dd($panierWithData);
-        $total = 0;
-
-        foreach ($panierWithData as $item){
-            $totalItem = $item['watch']->getPrice() * $item['quantity'];
-            $total += $totalItem;
-        }
-
 
         return $this->render('cart/panier.html.twig', [
-            'items'=>$panierWithData,
-            'total'=>$total
+            'items'=> $cartService->getFullCart(),
+            'total'=>$cartService->getTotal()
         ]);
     }
 
@@ -47,19 +27,29 @@ class CartController extends AbstractController
     public function add($id,CartService $cartService)
     {
         $cartService->add($id);
-        return $this->redirectToRoute("cart");
+
+        return $this->redirectToRoute("boutique");
     }
 
     /**
      * @Route("/cart/remove/{id}", name="remove_cart")
      */
-    public function remove($id, SessionInterface $session){
-        $panier = $session->get('panier', []);
-        if(!empty($panier[$id])){
-            unset($panier[$id]);
-        }
-
-        $session->set('panier',$panier);
+    public function remove($id, CartService $cartService){
+        $cartService->remove($id);
         return $this->redirectToRoute("cart");
     }
+
+
+//    public function totalQuantity(CartService $cartService)
+//    {
+//
+//        return $this->render('cart/panier.html.twig', [
+//            'totalQuantity'=> $cartService->getTotalQuantity(),
+//        ]);
+//
+//        return $this->render('base.html.twig', [
+//            'totalQuantity'=> $cartService->getTotalQuantity(),
+//        ]);
+//    }
+
 }
--- src/Service/cart/CartService.php
+++ src/Service/cart/CartService.php
@@ -2,15 +2,18 @@
 
 namespace App\Service\cart;
 
+use App\Repository\ProductRepository;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 
 class CartService{
 
     protected $session;
+    protected $productRepository;
 
-    public function __construct(SessionInterface $session)
+    public function __construct(SessionInterface $session, ProductRepository $productRepository)
     {
         $this->session = $session;
+        $this->productRepository = $productRepository;
     }
 
     public function add(int $id)
@@ -25,7 +28,45 @@ class CartService{
 
         $this->session->set('panier', $panier);
     }
-    public function remove(int $id){}
-    public function getFullCart() : array{}
-    public function getTotal() : float{}
+
+    public function remove(int $id)
+    {
+        $panier = $this->session->get('panier', []);
+        if(!empty($panier[$id])){
+            unset($panier[$id]);
+        }
+        $this->session->set('panier',$panier);
+    }
+
+    public function getFullCart() : array{
+
+        $panier = $this->session->get('panier',[]);
+        $panierWithData = [];
+
+        foreach ($panier as $id => $quantity){
+            $panierWithData[] = [
+                'watch'=> $this->productRepository->find($id),
+                'quantity'=>$quantity
+            ];
+        }
+        return $panierWithData;
+    }
+
+    public function getTotal() : float{
+        $total = 0;
+
+        foreach ($this->getFullCart() as $item){
+           $total += $item['watch']->getPrice() * $item['quantity'];
+        }
+        return $total;
+    }
+
+//    public function getTotalQuantity() : int {
+//        $totalQuantity = 0;
+//
+//        foreach ($this->getFullCart() as $item){
+//            $totalQuantity += $item['quantity'];
+//        }
+//        return $totalQuantity;
+//    }
 }
