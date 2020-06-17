769c007f05a99b3fcacc292ed77653e739488514
Leila Ech-Chabi <leila.echchabi@gmail.com>
2020-06-17 13:42:12 +0200

    PaymentForm

---
+++ public/js/sripe.js
@@ -0,0 +1,54 @@
+var stripe = Stripe('pk_test_51GtkFUFdOQZQgrage1TTqgmn50d5rxhL5nF2UKLPf9NbpYHoB9BotKqOK7VOZGYzzeoaJOf0GBNc2rAsgUNekX5x00Rcmym23O');
+var elements = stripe.elements();
+
+var style = {
+    base: {
+        fontSize: '16px',
+        color: '#32325d',
+    },
+};
+
+// Create an instance of the card Element.
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>.
+card.mount('#card-element');
+
+card.addEventListener('change', function(event) {
+    var displayError = document.getElementById('card-errors');
+    if (event.error) {
+        displayError.textContent = event.error.messages;
+    } else {
+        displayError.textContent = '';
+    }
+});
+
+// Create a token or display an error when the form is submitted.
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    stripe.createToken(card).then(function(result) {
+        if (result.error) {
+            // Inform the customer that there was an error.
+            var errorElement = document.getElementById('card-errors');
+            errorElement.textContent = result.error.message;
+        } else {
+            // Send the token to your server.
+            stripeTokenHandler(result.token);
+        }
+    });
+});
+
+function stripeTokenHandler(token) {
+    // Insert the token ID into the form so it gets submitted to the server
+    var form = document.getElementById('payment-form');
+    var hiddenInput = document.createElement('input');
+    hiddenInput.setAttribute('type', 'hidden');
+    hiddenInput.setAttribute('name', 'stripeToken');
+    hiddenInput.setAttribute('value', token.id);
+    form.appendChild(hiddenInput);
+
+    // Submit the form
+    form.submit();
+}
